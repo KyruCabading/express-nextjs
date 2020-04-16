e2f916bfe372ab412f15a3bc0ab0370bd5419cb7
Kyru Cabading <kyrusri@gmail.com>
2020-04-16 14:29:55 -0700

    handlebars: add handlebars engine

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const exphbs = require("express-handlebars");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -9,12 +10,19 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
+  server.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  server.set("view engine", "handlebars");
+
   server.get("/a", (req, res) => {
     res.send("A");
   });
 
   server.get("/b", (req, res) => {
     return app.render(req, res, "/b", req.query);
+  });
+
+  server.get("/home", function (req, res) {
+    res.render("home");
   });
 
   server.all("*", (req, res) => {
